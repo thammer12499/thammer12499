8ae221bb96f958435f6239278fa80996dba18905
thammer12499 <thammer12499@gmail.com>
2023-10-11 17:11:34 -0400

    Updating for JS assignment 2

--- docs/jscriptInter.js
+++ docs/jscriptInter.js
@@ -28,3 +28,47 @@ function changeColor(){
 function removeBackground(){
     document.body.style.backgroundImage = "none";
 }
+
+// Toggle Functions
+
+function fieldinit(){
+    var fieldset = document.getElementsByTagName('input');
+    for (var i = 0; i < fieldset.length; i++) {
+        fieldset[i].addEventListener('click', toggle, false);
+    }
+}
+
+function toggle (){
+    var id = this.id;
+    switch (id) {
+        case "RPGtoggle": {
+            var rpgs = document.getElementsByClassName ("RPG");
+            for (var i = 0; i < places.length; i++){
+                rpgs[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "FPStoggle": {
+            var fpses = document.getElementsByClassName ("FPS");
+            for (var i = 0; i < places.length; i++){
+                fpses[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "SurHortoggle": {
+            var sH = document.getElementsByClassName ("SurHor");
+            for (var i = 0; i < places.length; i++){
+                sH[i].classList.toggle("on")
+            }
+        };
+        break;
+        case "FarmSimtoggle": {
+            var farmsim = document.getElementsByClassName ("Farm");
+            for (var i = 0; i < places.length; i++){
+                farmsim[i].classList.toggle("on")
+            }
+        };
+        break;
+    }
+}
+window.onload = fieldinit;
